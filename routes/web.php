8943a565ebeb636097620ec8b98048e5de29f9c6
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-05-16 10:25:26 +1000

    Update web.php
    Added route to /products endpoint to pass in the ProductsController class and to perform the index function

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+//Laravel 8(New)
+Route::get('/products', [ProductsController::class, 'index']);
 
+//Laravel
 
 
 //firstproject.com == /
