8bbc716cf4b60d7bb6e7fd9d6a6498fe1744d76d
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-05-16 12:51:37 +1000

    Update ProductsController.php
    Removed variable title from view
    Added for each to loop through data array and show items as paragraph

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,14 +9,19 @@ class ProductsController extends Controller
     public function index(){
         $title = "Welcome to my Laravel 8 course";
         $description = "Created by Tanimara";
-
+        $data = [
+            'productOne' => 'Opel',
+            'productTwo' => 'Toyota'
+        ];
 
         // //Compact method
         // return view('products.index', 
         // compact('title', 'description'));
 
         // With method (better when passing single variable)
-        return view ('products.index') -> with('title', $title);
+        // return view ('products.index') -> with('title', $title);
+        return view ('products.index') -> with('data', $data);
+        
         
     }
 
