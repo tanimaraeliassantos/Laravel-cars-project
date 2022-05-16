cfb9486713febf4b58ddfb3f082a63811d864a91
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-05-16 15:40:48 +1000

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,25 +1,32 @@
 
-    <!-- Navigation -->
-    <nav class="top-menu-container">
-        <div class="logo-header">
-            <a href="">
-                <img
-                src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png"
-                alt="Logo personal portfolio"
-                title="Logo personal portfolio"
-                />
-            </a>
-        </div>
+<!-- Navigation -->
+<nav class="top-menu-container">
+    <div class="logo-header">
+        <a href="">
+            <img
+            src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png"
+            alt="Logo personal portfolio"
+            title="Logo personal portfolio"
+            />
+        </a>
+    </div>
 
-        <ul>
-            <li>
-                <a href="">About</a>
-            </li>
-            <li>
-                <a href="">Portfolio</a>
-            </li>
-            <li>
-                <a href="">Contact</a>
-            </li>
-        </ul>
-    </nav>
+    <ul>
+        <li>
+            <a 
+                href="/"
+                class="{{ request()->is('/') ? 'active' : '' }}">Home</a>
+        </li>
+        <li>
+            <a 
+                href="about"
+                class="{{  request()->is('about') ? 'active' : '' }}">About</a>
+        </li>
+        <li>
+            <a href="portfolio">Portfolio</a>
+        </li>
+        <li>
+            <a href="contact">Contact</a>
+        </li>
+    </ul>
+</nav>
