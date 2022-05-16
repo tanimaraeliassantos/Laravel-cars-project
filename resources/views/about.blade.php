2f2cc1d2978f3abc7f6df8838bd7c23b346bfb00
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-05-16 16:34:26 +1000

    Update about.blade.php
    Created for loop, foreach loop, for else loop and while loop

---
+++ resources/views/about.blade.php
@@ -0,0 +1,25 @@
+<!--
+    For loop
+    Foreach loop
+    For else loop
+    While loop
+-->
+
+{{-- @for($i=0; $i <= 10; $i++)
+    <h2>The number is {{ $i }}</h2>
+@endfor
+
+@foreach ($names as $name)
+<h2>The name is {{ $name }}</h2>
+@endforeach
+
+@forelse ($names as $name)
+    <h2>The name is {{ $name }}</h2>
+@empty
+    <h2>There are no names!</h2>
+@endforelse --}}
+
+{{ $i = 0 }}
+@while ($i < 10)
+    <h2>{{ $i++ }}</h2>
+@endwhile
