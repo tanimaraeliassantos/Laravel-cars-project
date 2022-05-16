790faa609a10693163129477a69fccc2324eeb23
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-05-16 16:14:11 +1000

    Update about.blade.php
    Removed previous code
    Added switch blade directive to show on view if variable name equals different case scenarios. If there's no match, then show the default message.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,15 @@
-@if (5 > 10)
-    <p>5 is lower than 10</p>
-@elseif (5 == 10)
-<p>5 is indeed lower than 10</p>
-@else
-<h2>All conditions are wrong!</h2>
-@endif
-
-@unless (empty($name))
-    <h2>Name isn't empty!</h2>
-@endunless
-
-@empty($secondName)
-    <h2>Name is empty!</h2>
-@endempty
-
-@isset($name)
-    <h2>Name has been set</h2>
-@endisset
+@switch($name)
+    @case('Amy')
+        <h2>Name is Tanimara!</h2>
+        @break
+    @case('Maria')
+        <h2>Name is Maria!</h2>
+        @break
+    @case('Tanica')
+        <h2>Name is Tanica!</h2>
+        @break
+    @default
+        <h2>No match found!</h2>
+            @break
+        
+@endswitch
