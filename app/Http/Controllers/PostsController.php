ae911433f9ba15a991da69c72d7d2af27994df58
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-05-17 16:08:08 +1000

    Update PostsController.php
    Added query to show on view the posts with id 1

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts/index');
+      $posts = DB::select('select * from posts WHERE id = 1');
+
+      dd($posts);
     }
 }
