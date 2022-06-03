753c1d957b410ae1925348130211a6c5886a4961
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-03 17:19:00 +1000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PagesController;
-use App\Http\Controllers\PostsController;
+use App\Http\Controllers\CarsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +16,4 @@ use App\Http\Controllers\PostsController;
 */
 
 
-Route::get('/', [PagesController::class, 'index']);
-Route::get('/about', [PagesController::class, 'about']);
-
-//Posts endpoint
-Route::get('/posts', [PostsController::class, 'index']);
+Route::resource('/', CarsController::class);
