99d614d4b69d526da4a111e062b3e69093318cd1
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-06 16:44:06 +1000

    Update tailwind.config.js

--- cars/tailwind.config.js
+++ cars/tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  content: ["./resources/**/*.blade.php",
-  "./resources/**/*.js",
-  "./resources/**/*.vue",],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./resources/**/**/*.blade.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
