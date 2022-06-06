72b5dd673eabcc51e6c54b116c8009fe50535d23
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-07 09:03:47 +1000

    Update index.blade.php

--- cars/resources/views/cars/index.blade.php
+++ cars/resources/views/cars/index.blade.php
@@ -8,4 +8,25 @@
             </h1>
         </div>
     </div>
+
+    <div class="w-5/6 py-10">
+        <div class="m-auto">
+                <span class="uppercase text-blue-500 font-bold text-xs italic">
+                    Founded: 2020
+                </span>
+
+                <h2 class="text-gray-700 text-5xl">
+                    Audi
+                </h2>
+
+                <p class="text-lg text-gray-700 py-6">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Possimus beatae tempora quis aut ea repudiandae, velit provident,
+                    asperiores officia esse cumque quos accusantium corrupti,
+                    corporis maxime illo deserunt. Dolore, odit.
+                </p>
+
+                <hr class="mt-4 mb-8">
+        </div>
+    </div>
 @endsection
