83c0ae0c84b48461fb26f58a11e2a38817e9d94b
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-06 10:33:57 +1000

    Update index.blade.php

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,2 +1,11 @@
 @extends('layouts.app')
-CARS
+
+@section('content')
+    <div class="m-auto w-4/5 py-24">
+        <div class='text-center'>
+            <h1 class="text-5xl uppercase bold">
+                Cars
+            </h1>
+        </div>
+    </div>
+@endsection
