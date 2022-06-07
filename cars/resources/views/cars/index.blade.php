c842aecd2cd7a32ffa3f9e18a72c733f381eaa2a
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-07 10:16:07 +1000

    Update index.blade.php
    Removed variables from loop for the firstOrFail method won't allow it to be shown here.

--- cars/resources/views/cars/index.blade.php
+++ cars/resources/views/cars/index.blade.php
@@ -8,20 +8,20 @@
             </h1>
         </div>
     </div>
-
+    {{ $cars }}
     <div class="w-5/6 py-10">
         @foreach ($cars as $car)
         <div class="m-auto">
             <span class="uppercase text-blue-500 font-bold text-xs italic">
-                Founded: {{ $car->founded }}
+                {{-- Founded: {{ $car->founded }} --}}
             </span>
 
             <h2 class="text-gray-700 text-5xl">
-                {{ $car->name }}
+                {{-- {{ $car->name }} --}}
             </h2>
 
             <p class="text-lg text-gray-700 py-6">
-                {{ $car->description }}
+                {{-- {{ $car->description }} --}}
             </p>
 
             <hr class="mt-4 mb-8">
