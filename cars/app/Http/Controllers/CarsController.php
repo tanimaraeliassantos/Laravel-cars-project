ecd886115f5eec7ba585afa9c359c1e9eef6e221
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-07 10:02:31 +1000

    Update CarsController.php
    Added method to fetch all cars from database and display it on view

--- cars/app/Http/Controllers/CarsController.php
+++ cars/app/Http/Controllers/CarsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Car;
 
 class CarsController extends Controller
 {
@@ -13,7 +14,11 @@ class CarsController extends Controller
      */
     public function index()
     {
-        return view('cars.index');
+        $cars = Car::all();
+        
+        return view('cars.index', [
+            'cars' =>$cars
+        ]);
     }
 
     /**
