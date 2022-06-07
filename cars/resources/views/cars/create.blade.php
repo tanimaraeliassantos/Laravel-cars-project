0aa8ff68b49748d7c852e6781b137b22bcaaef48
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-07 13:54:51 +1000

    Update create.blade.php

--- cars/resources/views/cars/create.blade.php
+++ cars/resources/views/cars/create.blade.php
@@ -1,1 +1,11 @@
-CARS
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <div>
+            <h1 class="text-5xl uppercase bold">
+                Create car
+            </h1>
+        </div>
+    </div>
+@endsection
