b48f03007d76e10d55bd404780bc67ebae6ca110
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-07 10:05:16 +1000

    Update index.blade.php
    Added foreach loop to go through each row of table to print them on display.

--- cars/resources/views/cars/index.blade.php
+++ cars/resources/views/cars/index.blade.php
@@ -10,23 +10,22 @@
     </div>
 
     <div class="w-5/6 py-10">
+        @foreach ($cars as $car)
         <div class="m-auto">
-                <span class="uppercase text-blue-500 font-bold text-xs italic">
-                    Founded: 2020
-                </span>
+            <span class="uppercase text-blue-500 font-bold text-xs italic">
+                Founded: {{ $car->founded }}
+            </span>
 
-                <h2 class="text-gray-700 text-5xl">
-                    Audi
-                </h2>
+            <h2 class="text-gray-700 text-5xl">
+                {{ $car->name }}
+            </h2>
 
-                <p class="text-lg text-gray-700 py-6">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Possimus beatae tempora quis aut ea repudiandae, velit provident,
-                    asperiores officia esse cumque quos accusantium corrupti,
-                    corporis maxime illo deserunt. Dolore, odit.
-                </p>
+            <p class="text-lg text-gray-700 py-6">
+                {{ $car->description }}
+            </p>
 
-                <hr class="mt-4 mb-8">
-        </div>
+            <hr class="mt-4 mb-8">
+    </div>
+        @endforeach
     </div>
 @endsection
