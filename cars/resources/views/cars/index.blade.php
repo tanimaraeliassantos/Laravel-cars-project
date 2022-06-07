d27ebcd4ff73d7bd59754b17481b08cb60e7a0ed
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-07 10:56:46 +1000

    Update index.blade.php
    Added button to cars page that links to the create page.

--- cars/resources/views/cars/index.blade.php
+++ cars/resources/views/cars/index.blade.php
@@ -8,20 +8,28 @@
             </h1>
         </div>
     </div>
-    {{ $cars->name }}
+
+    <div class="pt-10">
+        <a
+            href="cars/create"
+            class="border-b-2 pb-2 border-dotted italic text-gray-500">
+            Add a new car &rarr;
+        </a>
+    </div>
+   
     <div class="w-5/6 py-10">
         @foreach ($cars as $car)
         <div class="m-auto">
             <span class="uppercase text-blue-500 font-bold text-xs italic">
-                {{-- Founded: {{ $car->founded }} --}}
+                Founded: {{ $car->founded }}
             </span>
 
             <h2 class="text-gray-700 text-5xl">
-                {{-- {{ $car->name }} --}}
+                {{ $car->name }}
             </h2>
 
             <p class="text-lg text-gray-700 py-6">
-                {{-- {{ $car->description }} --}}
+                {{ $car->description }}
             </p>
 
             <hr class="mt-4 mb-8">
