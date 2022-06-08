e876c4da954e6b451e2062aa855671f3d267ceaf
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-08 13:32:55 +1000

    Update create.blade.php

--- cars/resources/views/cars/create.blade.php
+++ cars/resources/views/cars/create.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <div>
+    <div class="m-auto w-4/8 py-24">
+        <div class="text-center">
             <h1 class="text-5xl uppercase bold">
                 Create car
             </h1>
         </div>
+    </div>
+
+    <div class="flex justify-center pt-20">
     </div>
 @endsection
