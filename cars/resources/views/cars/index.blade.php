59011649cbc211e9e4e8d1c4aff2e897e550801c
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-15 14:36:40 +1000

    Update index.blade.php
    Render array of data on index view

--- cars/resources/views/cars/index.blade.php
+++ cars/resources/views/cars/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@foreach($cars as $car)
+    {{ $car['name'] }}
+@endforeach
+
 @section('content')
     <div class="m-auto w-4/5 py-24">
         <div class='text-center'>
