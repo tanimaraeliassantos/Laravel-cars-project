070a19118e7a631bc8463e61bc39e357a92bb042
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-15 16:44:49 +1000

    Update show.blade.php

--- cars/resources/views/cars/show.blade.php
+++ cars/resources/views/cars/show.blade.php
@@ -19,6 +19,22 @@
                 {{ $car->description }}
             </p>
 
+            <ul>
+                <p class-"text-lg text-gray-700 py-3">
+                    Models:
+                </p>
+
+                @forelse ($car->carModels as $model)
+                    <li class="inline italix text-gray-600 px-1 py-6">
+                        {{ $model['model_name'] }}
+                    </li>
+                @empty
+                    <p>
+                        No models found.
+                    </p>
+                @endforelse
+            </ul>
+
 
 
             <hr class="mt-4 mb-8">
