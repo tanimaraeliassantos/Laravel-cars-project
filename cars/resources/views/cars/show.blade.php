3cd6d515b7a00316cecc008e47a6df78ef7c6465
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-20 09:14:06 +1000

    Update show.blade.php
    Added table to show car models and engines on show view

--- cars/resources/views/cars/show.blade.php
+++ cars/resources/views/cars/show.blade.php
@@ -6,10 +6,6 @@
             <h1 class="text-5xl uppercase bold">
                 {{ $car->name }}
             </h1>
-
-            <p class="text-lg text-gray-700 py-6">
-                {{ $car->headquarter->headquarters }}, {{ $car->headquarter->country }}
-            </p>
         </div>
     </div>
     <div class="py-10 text-center">
@@ -23,21 +19,35 @@
                 {{ $car->description }}
             </p>
 
-            <ul>
-                <p class-"text-lg text-gray-700 py-3">
-                    Models:
-                </p>
-
+            <table class="table-auto">
+                <tr class="bg-blue-100">
+                    <th class="w-1/2 border-4 border-gray-500">
+                        Model
+                    </th>
+                    <th class="w-1/2 border-4 border-gray-500">
+                        Engines
+                    </th>
+                </tr>
                 @forelse ($car->carModels as $model)
-                    <li class="inline italix text-gray-600 px-1 py-6">
-                        {{ $model['model_name'] }}
-                    </li>
+                    <tr>
+                        <td class="border-4 border-gray-500">
+                            {{ $model->model_name }}
+                        </td>
+                        <td class="border-4 border-gray-500">
+                            @foreach ($car->engines as $engine)
+                                @if ($model->id == $engine->model_id)
+                                    {{ $engine->engine_name }}
+                                @endif
+                            @endforeach
+                        </td>
+                    </tr>
                 @empty
                     <p>
-                        No models found.
+                        No car models found!
                     </p>
                 @endforelse
-            </ul>
+
+            </table>
 
 
 
