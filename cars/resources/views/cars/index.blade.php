3087c96a2614abe093498c143c03e174d184c9d3
Tanimara Santos <36615712+tanimaraeliassantos@users.noreply.github.com>
2022-06-23 16:04:22 +1000

    Update index.blade.php
    Added pagination

--- cars/resources/views/cars/index.blade.php
+++ cars/resources/views/cars/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
-{{-- @foreach($cars as $car)
+{{-- @foreach ($cars as $car)
     {{-- {{ $car['name'] }} --}}
-{{-- @endforeach  --}}
+{{-- @endforeach --}}
 
 @section('content')
     <div class="m-auto w-4/5 py-24">
@@ -14,49 +14,44 @@
     </div>
 
     <div class="pt-10">
-        <a
-            href="cars/create"
-            class="border-b-2 pb-2 border-dotted italic text-gray-500">
+        <a href="cars/create" class="border-b-2 pb-2 border-dotted italic text-gray-500">
             Add a new car &rarr;
         </a>
     </div>
-   
+
     <div class="w-5/6 py-10">
         @foreach ($cars as $car)
-        <div class="m-auto">
-            <div class="float-right">
-                <a 
-                    class="border-b-2 pb-2 border-dotted italic text-green-500"
-                    href="cars/{{ $car->id }}/edit">
+            <div class="m-auto">
+                <div class="float-right">
+                    <a class="border-b-2 pb-2 border-dotted italic text-green-500" href="cars/{{ $car->id }}/edit">
                         Edit &rarr;
-                </a>
+                    </a>
 
-                <form action="/cars/{{ $car->id }}" class="pt-3" method="POST">
-                    @csrf
-                    @method('delete')
-                    <button
-                        type="submit"
-                        class="border-b-2 pb-2 border-dotted italic text-red-500">
+                    <form action="/cars/{{ $car->id }}" class="pt-3" method="POST">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="border-b-2 pb-2 border-dotted italic text-red-500">
                             Delete &rarr;
-                    </button>
-                </form>
+                        </button>
+                    </form>
+                </div>
+                <span class="uppercase text-blue-500 font-bold text-xs italic">
+                    Founded: {{ $car->founded }}
+                </span>
+
+                <h2 class="text-gray-700 text-5xl hover:text-gray-500">
+                    <a href="/cars/{{ $car->id }}">
+                        {{ $car->name }}
+                    </a>
+                </h2>
+
+                <p class="text-lg text-gray-700 py-6">
+                    {{ $car->description }}
+                </p>
+
+                <hr class="mt-4 mb-8">
             </div>
-            <span class="uppercase text-blue-500 font-bold text-xs italic">
-                Founded: {{ $car->founded }}
-            </span>
-
-            <h2 class="text-gray-700 text-5xl hover:text-gray-500">
-                <a href="/cars/{{ $car->id }}">
-                    {{ $car->name }}
-                </a>
-            </h2>
-
-            <p class="text-lg text-gray-700 py-6">
-                {{ $car->description }}
-            </p>
-
-            <hr class="mt-4 mb-8">
-    </div>
         @endforeach
     </div>
+    {{ $cars->links() }}
 @endsection
